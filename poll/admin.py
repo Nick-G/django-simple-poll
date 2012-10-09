5c36372735f27870d62f228f831363d096692f94
Nick Guzy <guziy.nick@gmail.com>
2012-10-09 09:12:12 +0300

    Edit admin and add fixture

--- poll/admin.py
+++ poll/admin.py
@@ -1,27 +1,20 @@
 from django.contrib import admin
 from poll.models import *
-from django.utils.translation import gettext as _
-from hvad.admin import TranslatableAdmin
+from hvad.admin import TranslatableAdmin, TranslatableTabularInline
 
-class PollItemInline(admin.TabularInline):
+
+class PollItemInline(TranslatableTabularInline):
     model = Item
     extra = 0
     max_num = 15
 
+
 class PollAdmin(TranslatableAdmin):
-    #list_display = ('title', 'date', 'vote_count', 'is_published')
-    #inlines = [PollItemInline,]
-    pass
+    inlines = [PollItemInline, ]
+
 
 class ItemAdmin(TranslatableAdmin):
-	pass
+    pass
 
 admin.site.register(Item, ItemAdmin)
 admin.site.register(Poll, PollAdmin)
-
-
-# class VoteAdmin(admin.ModelAdmin):
-#     list_display = ('poll', 'ip', 'user', 'datetime')
-#     list_filter = ('poll', 'datetime')
-# 
-# admin.site.register(Vote, VoteAdmin)
